ea05fc15e4d89fba82ea2d893605ec7fa65d05ff
ND_LJQ <2206101625@qq.com>
2022-12-17 00:14:35 +0800

    :sparkles: feature: 配置分布式(增加mysql连接,客户端配置)

---
+++ forum_server_consumer/src/main/java/edu/hunau/consumerApp.java
@@ -0,0 +1,19 @@
+package edu.hunau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class consumerApp {
+
+    @Bean
+    @LoadBalanced //3 负载均衡注解模式下需要的注解 其他2种不需要
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(consumerApp.class,args);
+    }
+}
