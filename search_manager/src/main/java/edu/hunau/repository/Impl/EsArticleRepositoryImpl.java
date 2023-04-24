1e064d140ee14b122e60f255a7833ed7a5c1e0ba
ND_LJQ <2206101625@qq.com>
2023-04-24 16:58:38 +0800

    :bug: feature(search_manager): 搜索接口健壮性调整

--- search_manager/src/main/java/edu/hunau/repository/Impl/EsArticleRepositoryImpl.java
+++ search_manager/src/main/java/edu/hunau/repository/Impl/EsArticleRepositoryImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 
 
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
